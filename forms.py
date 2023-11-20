c6788c53f80e9dc8814f15906450ee6bc39d30b8
ananyab903 <148983076+ananyab903@users.noreply.github.com>
2023-11-19 23:56:09 -0800

    20/11/2023

--- forms.py
+++ forms.py
@@ -1,8 +1,20 @@
-# yourapp/forms.py
 from django import forms
-from .models import Student
+from .models import Semester
 
-class StudentForm(forms.ModelForm):
+class SemesterForm(forms.ModelForm):
     class Meta:
-        model = Student
-        fields = ['tenth_mark_card', 'twelfth_mark_card', 'aadhar_card', 'admission_order']
+        model = Semester
+        fields = ['usn','semester_number', 'marks_obtained', 'total_marks']
+
+    SEMESTER_CHOICES = [
+        ('1', 'Semester 1'),
+        ('2', 'Semester 2'),
+        ('3', 'Semester 3'),
+        ('4', 'Semester 4'),
+        ('5', 'Semester 5')
+        # Add more as needed...
+    ]
+    usn=forms.CharField(max_length=10)
+    semester_number = forms.ChoiceField(choices=SEMESTER_CHOICES)
+    marks_obtained = forms.DecimalField(max_digits=5, decimal_places=2)
+    total_marks = forms.DecimalField(max_digits=5, decimal_places=2)
