2f282adbd2c4764f21a3484375fec0671dcd426f
Ganavi02 <151150525+Ganavi02@users.noreply.github.com>
2023-11-20 12:44:12 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 # yourapp/urls.py
 from django.urls import path
-from .views import upload_student_files, view_uploaded_file
+from .views import upload_student_files
 
 urlpatterns = [
     path('upload/', upload_student_files, name='upload_student_files'),
