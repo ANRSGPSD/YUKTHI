32afd34002bcffa4718c9b1a727cf79ad4687a25
SpoorthiS29 <138692239+SpoorthiS29@users.noreply.github.com>
2023-11-22 16:23:10 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
-# yourappname/urls.py
+# myapp/urls.py
 from django.urls import path
-from App import views
+from .views import register, user_login, user_page
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    # Add other URLs as needed
+    path('register/', register, name='register'),
+    path('user_login/', user_login, name='user_login'),
+    path('user_page/', user_page, name='user_page'),
+    
 ]
