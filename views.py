630e11980187b3bbe76c5042544aa51c97e33980
SpoorthiS29 <138692239+SpoorthiS29@users.noreply.github.com>
2023-11-22 16:23:51 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,27 +1,48 @@
 # yourappname/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm, LoginForm
-def home(request):
-    return render(request, 'home.html')
+from .forms import UserRegistrationForm, UserLoginForm
+
+def user_page(request):
+    return render(request, 'user_page.html')
+
 def register(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
-            return redirect('home')  # Redirect to your home page
+
+            # Authenticate the user
+            authenticated_user = authenticate(
+                request,
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],  # Ensure you have a password field in your form
+            )
+
+            # Log in the user if authentication is successful
+            if authenticated_user:
+                login(request, authenticated_user)
+                return redirect('user_page')  # Redirect to your home page
+
     else:
-        form = SignUpForm()
+        form = UserRegistrationForm()
+
     return render(request, 'register.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
-        form = LoginForm(request, request.POST)
+        form = UserLoginForm(request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')  # Redirect to your home page
+            user = authenticate(
+                request,
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user:
+                login(request, user)
+                return redirect('user_page')  # Redirect to your home page
+
     else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
+        form = UserLoginForm()
+
+    return render(request, 'user_login.html', {'form': form})
